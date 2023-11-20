a38568b1a831dfa14c7d4f317af848f3b6008509
Kamran Zafar <kamranzafar4343@gmail.com>
2023-11-20 22:40:06 +0500

    npm; art figlet

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-//using npm
-var oneLinerJoke = require('one-liner-joke');
+// //using npm
+// var oneLinerJoke = require('one-liner-joke');
 
-console.log(oneLinerJoke.getRandomJoke());
+// console.log(oneLinerJoke.getRandomJoke());
+
+//using npm 2nd time
+var figlet = require("figlet");
+
+figlet("Mr Kamran", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
