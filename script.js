a4a8ddd51354889e6dc89ab8105d12902129ca95
Kamran Zafar <kamranzafar4343@gmail.com>
2023-11-20 23:08:50 +0500

    running express; making routes

--- script.js
+++ script.js
@@ -4,13 +4,23 @@
 // console.log(oneLinerJoke.getRandomJoke());
 
 //using npm 2nd time
-var figlet = require("figlet");
+// var figlet = require("figlet");
 
-figlet("Mr Kamran", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
-});
+// figlet("Mr Kamran", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+// var dafar = "tayyab";
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.listen(3000)
